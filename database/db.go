1d48cdf5f6c4350fddbb13a51024b7cbb31416eb
Amoo Olusola Simeon <aosimeon@outlook.com>
2023-07-09 16:29:30 +0100

    first commit

--- database/db.go
+++ database/db.go
@@ -2,14 +2,14 @@ package database
 
 import (
 	"fmt"
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 const DbUsername = "admin"
 const DbPassword = "password"
 const DbName = "shortner"
-const DbHost = "localhost"
+const DbHost = "dpg-cild24lgkuvinfk5eqtg-a"
 const DbPort = "3306"
 
 var Db *gorm.DB
@@ -22,9 +22,10 @@ func InitDb() *gorm.DB {
 
 // connectDB - Connect to database
 func connectDB() *gorm.DB {
-	dsn := DbUsername + ":" + DbPassword + "@tcp" + "(" + DbHost + ":" + DbPort + ")/" + DbName + "?" + "parseTime=true&loc=Local"
-	fmt.Println("dsn : ", dsn)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// dsn := DbUsername + ":" + DbPassword + "@tcp" + "(" + DbHost + ":" + DbPort + ")/" + DbName + "?" + "parseTime=true&loc=Local"
+	dsn := "host=dpg-cild24lgkuvinfk5eqtg-a user=admin password=hVNuRlkX9RDx6gktWYJWtC5CRYWL7Lsd dbname=shortner port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		fmt.Printf("Error connecting to database : error=%v\n", err)
